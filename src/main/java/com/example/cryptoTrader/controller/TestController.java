26a49f58a611760a1196d0551c79c0db0e221cfd
Samuel <sam.tan95@hotmail.com>
2023-03-25 12:03:39 +0800

    add swaggerui

--- src/main/java/com/example/cryptoTrader/controller/TestController.java
+++ src/main/java/com/example/cryptoTrader/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.cryptoTrader.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestController {
+    @GetMapping
+    public ResponseEntity<?> testMethod() {
+        return null;
+    }
 }
