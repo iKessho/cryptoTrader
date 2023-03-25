66056efb49d57b5c2f25fdf1194048b3edc764ae
Samuel <sam.tan95@hotmail.com>
2023-03-25 11:57:08 +0800

    initial commit

---
+++ src/test/java/com/example/cryptoTrader/CryptoTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoTrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
